02bd3d59edfe7f5a61a1aa7ee6cbd788309002e5
daniellepelley@gmail.com <daniellepelley@gmail.com>
2015-06-22 23:20:24 +0100

    Missing file

---
+++ Spikes/SutBuilder.Test/MoqSutTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentTests.Core;
+using SutBuilder.Moq;
+
+namespace SutBuilder.Test
+{
+    public class MoqSutTests<T> : SutTests<T>
+        where T : class
+    {
+        public MoqSutTests()
+            :base(new MoqClassConstructor<T>(), new ConstructorProvider<T>())
+        { }
+
+        public IGiven<T> Given(T sut)
+        {
+            return new Given<T>();
+        }
+    }
+}
